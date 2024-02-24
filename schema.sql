4a1fa449e47c69ce6a0bd14131e7a9ecbb84ecb9
Denis Strizhkin <strdenis02@gmail.com>
2024-02-25 02:53:27 +0300

    added cars

--- schema.sql
+++ schema.sql
@@ -39,27 +39,37 @@ create table manufacturer (
   name varchar(20)  not null unique
 );
 
-create table model (
-  id   int unsigned auto_increment primary key,
-  code varchar(20)  not null unique,
-  name varchar(20)  not null
-);
+insert into manufacturer (name) values ('Mitsubishi');
+insert into manufacturer (name) values ('KIA');
+insert into manufacturer (name) values ('Hyundai');
 
-create table cars (
+create table model (
   id              int unsigned auto_increment primary key,
-  vin             varchar(17)  not null unique,
-  model_id        int unsigned not null,
+  name            varchar(20)  not null,
   manufacturer_id int unsigned not null,
 
-  foreign key (manufacturer_id) references manufacturer (id) on delete cascade,
-  foreign key (model_id) references model (id) on delete cascade
+  unique(name, manufacturer_id),
+
+  foreign key (manufacturer_id) references manufacturer (id) on delete cascade
 );
 
-create view cars_view as (
-  select cars.id, vin, cars.name, model.name as model, manufacturer.name as manufacturer
-  from cars
-    join model        on cars.model_id        = model.id
-    join manufacturer on cars.manufacturer_id = manufacturer.id
+insert into model (name, manufacturer_id) values (
+  'Sorento', (select id from manufacturer where name = 'KIA')
+);
+insert into model (name, manufacturer_id) values (
+  'Sportage', (select id from manufacturer where name = 'KIA')
+);
+insert into model (name, manufacturer_id) values (
+  'ASX', (select id from manufacturer where name = 'Mitsubishi')
+);
+insert into model (name, manufacturer_id) values (
+  'L200', (select id from manufacturer where name = 'Mitsubishi')
+);
+insert into model (name, manufacturer_id) values (
+  'Pajero III', (select id from manufacturer where name = 'Mitsubishi')
+);
+insert into model (name, manufacturer_id) values (
+  'Creta', (select id from manufacturer where name = 'Hyundai')
 );
 
 create table job (
@@ -69,35 +79,35 @@ create table job (
 );
 
 create table job_prices (
-  job_id int unsigned not null,
-  car_id int unsigned not null,
-  price  int unsigned not null,
+  job_id   int unsigned not null,
+  model_id int unsigned not null,
+  price    int unsigned not null,
 
-  unique(job_id, car_id),
+  unique(job_id, model_id),
 
-  foreign key (job_id) references job  (id) on delete cascade,
-  foreign key (car_id) references cars (id) on delete cascade
+  foreign key (job_id)   references job   (id) on delete cascade,
+  foreign key (model_id) references model (id) on delete cascade
 );
 
-create view jobs_prices_view as (
-  select job.name as job, cars.name as car, price
-  from job_prices
-    join job  on job_prices.job_id = job.id
-    join cars on job_prices.car_id = cars.id
-);
+-- create view jobs_prices_view as (
+--   select job.name as job, cars.name as car, price
+--   from job_prices
+--     join job  on job_prices.job_id = job.id
+--     join cars on job_prices.car_id = cars.id
+-- );
 
 create table orders (
   id         int unsigned auto_increment primary key,
   user_id    int unsigned not null,
-  car_id     int unsigned not null,
+  model_id   int unsigned not null,
   job_id     int unsigned not null,
   price      int unsigned not null,
   commentary text,
   created    datetime not null default current_timestamp,
 
-  unique(user_id, car_id, job_id, created),
+  unique(user_id, created),
 
-  foreign key (job_id)  references job   (id) on delete cascade,
-  foreign key (car_id)  references cars  (id) on delete cascade,
-  foreign key (user_id) references users (id) on delete cascade
+  foreign key (job_id)   references job   (id) on delete cascade,
+  foreign key (model_id) references model (id) on delete cascade,
+  foreign key (user_id)  references users (id) on delete cascade
 );
