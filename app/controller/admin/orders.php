c13e8877aed3d3bcd110c8f59c0c0cd0ac124623
Denis Strizhkin <strdenis02@gmail.com>
2024-03-01 17:01:04 +0300

    Orders list

--- app/controller/admin/orders.php
+++ app/controller/admin/orders.php
@@ -10,23 +10,23 @@ secure('admin');
 
 if (isset($_GET['delete'])) {
     try {
-        $job_prices = JobPrices::get($_GET['delete']);
-        if ($job_prices) {
-            set_message('Удалена цена ' . $job_prices->job() . ' | ' . $job_prices->model());
-            $job_prices->delete();
+        $orders = Orders::get($_GET['delete']);
+        if ($orders) {
+            set_message('Удален заказ ' . $orders->job() . ' | ' . $orders->model());
+            $orders->delete();
         } else {
-            set_message('Цена не найдена');
+            set_message('Заказ не найден');
         }
     } catch (Throwable $e) {
         set_message($e->getMessage());
     }
 }
 
-$job_prices = JobPrices::get_all();
+$orders = Orders::get_all();
 
-render_panel_page('admin/job_prices', [
-    'job_prices' => $job_prices,
-    'link_edit' => URLS::ADMIN_JOB_PRICES_EDIT,
-    'link_add' => URLS::ADMIN_JOB_PRICES_ADD,
-    'link_delete' => URLS::ADMIN_JOB_PRICES,
+render_panel_page('admin/orders', [
+    'orders' => $orders,
+    'link_edit' => URLS::ADMIN_ORDERS_EDIT,
+    'link_add' => URLS::ADMIN_ORDERS_ADD,
+    'link_delete' => URLS::ADMIN_ORDERS,
 ]);
