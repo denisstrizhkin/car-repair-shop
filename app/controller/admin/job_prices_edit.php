116a7b9144edf9863e286662047bb0465c6efd91
Denis Strizhkin <strdenis02@gmail.com>
2024-03-01 16:27:34 +0300

    Edit job prices

--- app/controller/admin/job_prices_edit.php
+++ app/controller/admin/job_prices_edit.php
@@ -8,27 +8,36 @@ include_once(__DIR__ . "/../../includes/functions.inc.php");
 
 secure('admin');
 
-$role = null;
+$job_price = null;
 if (!isset($_GET['id'])) {
-    set_message('Роль не выбрана');
+    set_message('Цена не выбрана');
 } else {
-    $role = role::get($_GET['id']);
-    if (!$role) {
-        set_message('Роли не существует');
+    $job_price = JobPrices::get($_GET['id']);
+    if (!$job_price) {
+        set_message('Цены не существует');
     }
 }
 
-if (isset($_POST['name'])) {
+if (isset($_POST['job_id'])) {
     try {
-        $role->set_name($_POST['name']);
-        $role->update();
-        set_message('Роль изменена ' . $role->name());
-        redirect(URLS::ADMIN_ROLES);
+        $job_price->set_job_id($_POST['job_id']);
+        $job_price->set_model_id($_POST['model_id']);
+        $job_price->set_price($_POST['price']);
+        $job_price->update();
+        set_message('Цена изменена ' . $job_price->job() . ' | ' . $job_price->model());
+        redirect(URLS::ADMIN_JOB_PRICES);
     } catch (Throwable $e) {
         set_message($e->getMessage());
     }
 }
 
-render_panel_page('admin/roles_edit', [
-    'role' => $role,
+$manufacturers = Manufacturer::get_all();
+$models = CarModel::get_all();
+$jobs = Job::get_all();
+
+render_panel_page('admin/job_prices_edit', [
+    'job_price' => $job_price,
+    'models' => $models,
+    'manufacturers' => $manufacturers,
+    'jobs' => $jobs,
 ]);
---
+++ static/models_populator.js
@@ -0,0 +1,24 @@
+"use strict";
+
+ const manufactuer_selector = document.getElementById("manufacturer_id");
+ let model_selector = document.getElementById("model_id");
+
+ const populate_model_selector = () => {
+   for (const [model_id, model] of Object.entries(models)) {
+     if (manufacturers[model.manufacturer] === manufactuer_selector.value) {
+       const opt = document.createElement("option");
+       opt.value = model_id;
+       opt.textContent = model.name;
+       if (current_model === model.manufacturer + ' | ' + model.name) {
+         opt.selected = true;
+       }
+       model_selector.appendChild(opt);
+     }
+   }
+ };
+
+ populate_model_selector();
+ manufactuer_selector.addEventListener('change', ev => {
+   model_selector.innerHTML = "";
+   populate_model_selector();
+ });
